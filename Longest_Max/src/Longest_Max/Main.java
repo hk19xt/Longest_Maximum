c9b2487eefad280850cddcac9c9b6319ee77b03d
Hyejin Kim <81273823+hk19xt@users.noreply.github.com>
2021-09-28 15:02:58 -0400

    Update Main.java

--- Longest_Max/src/Longest_Max/Main.java
+++ Longest_Max/src/Longest_Max/Main.java
@@ -1,10 +1,13 @@
 package Longest_Max;
 
+/*This is the main class.
+The users can change the input array and the size of the input array in the hardcoded line. */
+
 public class Main {
     public static void main(String[] args){
-        int userArray[] = {0,1,0,3,2,3};
-        int size = userArray.length;
-        Longest l = new Longest();
+        int userArray[] = {0,1,0,3,2,3}; //Users can change the input value here.
+        int size = userArray.length; //Users can change the input size of the elements here.
+        Longest l = new Longest(); //Initialize the new longest array.
         int output;
         output = l.Longest(userArray,size);
         System.out.println(output);
